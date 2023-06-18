2c7d51905247860bc19abf56a196f9bc6b313211
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-06-18 16:38:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(input) {
+  let input = "368";
+	let table = {
+		2:"abc", 
+	   3:"def", 
+	   4:"ghi", 
+		5:"jkl", 
+		6"mno" ,
+		7:"pqrs" ,
+		8:"tuv", 
+		9:"wxyz"
+	}
+let solution= [];
+function solve(currentIndex,calculatedString) {
+	if(input.length <= currentIndex){
+		solution.push(calculatedString);
+		return ;
+	}
+	let currentChar = input[currentIndex]
+	for(let i=0;i<table[currentChar].length;i++){
+		solve(currentIndex + 1, calculatedString + table[currentChar]);
+	}
+	
+}
+solve(0, "")
+return solution;
 }
 
 module.exports = letterCombinations;
