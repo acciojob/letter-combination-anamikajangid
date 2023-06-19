361a4b9e0247a3169f8e3db981e3e770236678ea
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-06-19 22:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-function letterCombinations(input) {
-  let input = "368";
-	let table = {
-		2:"abc", 
-	   3:"def", 
-	   4:"ghi", 
-		5:"jkl", 
-		6"mno" ,
-		7:"pqrs" ,
-		8:"tuv", 
-		9:"wxyz"
-	}
-let solution= [];
-function solve(currentIndex,calculatedString) {
-	if(input.length <= currentIndex){
-		solution.push(calculatedString);
-		return ;
-	}
-	let currentChar = input[currentIndex]
-	for(let i=0;i<table[currentChar].length;i++){
-		solve(currentIndex + 1, calculatedString + table[currentChar]);
-	}
-	
-}
-solve(0, "")
-return solution;
+function letterCombinations(digits) {
+  //Complete the function
+	const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  // Create an array to store the combinations
+  let combinations = [];
+
+  // Recursive function to generate combinations
+  function backtrack(combination, nextDigits) {
+    // Base case: if there are no more digits to check
+    if (nextDigits.length === 0) {
+      // Add the combination to the array
+      combinations.push(combination);
+    }
+    // Recursive case: iterate over the letters corresponding to the current digit
+    else {
+      const digit = nextDigits[0];
+      const letters = digitToLetters[digit];
+
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        // Append the current letter to the combination and move to the next digit
+        backtrack(combination + letter, nextDigits.slice(1));
+      }
+    }
+  }
+
+  // Start the backtracking process
+  if (digits.length !== 0) {
+    backtrack('', digits);
+  }
+
+  // Return the sorted array of combinations
+  return combinations.sort();
 }
 
+module.exports = letterCombinations;
 module.exports = letterCombinations;
